410ca24fd65b9d778412c15cc4176361db7647aa
nikhilONadrenaline <gamaxene1122@gmail.com>
2026-08-24 20:11:48 +0530

    Intialize spring boot backend

---
+++ arovia-backend/src/test/java/com/arovia/arovia_backend/AroviaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arovia.arovia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroviaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
