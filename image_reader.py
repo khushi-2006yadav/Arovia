ae63c8938e16f0c2217df8d73222badc76740b8b
sakshamagrawal9170680802-spec <sakshamagrawal9170680802@gmail.com>
2026-08-29 02:21:48 +0530

    Delete image_reader.py

--- image_reader.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-def read_image(image_path):
-    image=cv2.imread(image_path)
-    if(image is None):
-        raise FileNotFoundError(
-            f"could not read image from path : {image_path}"
-        )
-    return image
